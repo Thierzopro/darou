d07ff5edd1669ad4c20413c3001a5f2fecae526c
ThierzoPro <t.loum30@isepdiamniadio>
2022-11-23 14:07:53 +0000

    affichage mon projet js

--- style.js
+++
@@ -1,9 +0,0 @@
-const one =()=> promise.resolve("one")
-async function myFunc() {
-	console.log("in function!")
-	const res = avait one()
-	console.log(res)
-}
-console.log("before function")
-myFunc();
-console.log("after function")
